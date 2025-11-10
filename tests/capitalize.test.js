122399c8a61ce121ec7e53c886fa126df3f9210e
Carlos <carlosaac232001@gmail.com>
2025-11-10 17:27:31 -0500

    feat: create testing file

---
+++ tests/capitalize.test.js
@@ -0,0 +1,11 @@
+import { capitalize } from '../main';
+
+test('takes a string and returns it with the first character capitalized', () => {
+  const word1 = 'dog';
+  const word2 = 'house';
+  const word3 = 'car';
+
+  expect(capitalize(word1)).toBe('Dog');
+  expect(capitalize(word2)).toBe('House');
+  expect(capitalize(word3)).toBe('Car');
+});
