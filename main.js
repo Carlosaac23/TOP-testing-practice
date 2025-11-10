c0096a608b0b05fbac016bb561f966e0a801d025
Carlos <carlosaac232001@gmail.com>
2025-11-10 17:34:13 -0500

    feat: create reverseString function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 export function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
+
+export function reverseString(word) {
+  return word.split('').reverse().join('');
+}
